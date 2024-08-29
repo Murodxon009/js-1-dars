abe593f596180949241d374b429d6b8aeb091994
Murodxon009 <ozodxonovm@gmail.com>
2024-08-29 13:43:11 +0500

    done

---
+++ javascrip.js
@@ -0,0 +1,22 @@
+let meningismim = prompt("ismingizni kiriting");
+
+console.log("ism", meningismim);
+
+let meningfamiliyam = prompt("Familiyangizni kiriting");
+
+console.log("familyangiz", meningfamiliyam);
+
+let meninyowim = prompt("Yoshingizni kiriting");
+
+console.log("yoshimgiz", meninyowim);
+
+let meningyoqtirganfanim = prompt("Yoqtirgan faningizni kiriting");
+
+console.log("faningiz", meningyoqtirganfanim);
+
+let meningyashaydigantumaningizni = prompt("Yashaydigan tumaningizi kiriting");
+
+console.log("yashaydigan tumaningiz ", meningyawaydigantumaningizni);
+
+alert('Malumotlaringizni kiritganingiz uchun tashakur')
+ 
